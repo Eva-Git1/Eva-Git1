cf91b204e37cb87d19aef30123201083bba75e5e
Eva-Git1 <evaassi92140@gmail.com>
2025-02-27 13:48:03 +0100

    Develop

---
+++ Script.java
@@ -0,0 +1,162 @@
+<script src="/files/theme/MutationObserver.js"></script>
+*// première syntax.
+<script async="" src="//cdn2.editmysite.com/js/wsnbn/snowday262.js"></script><script type="text/javascript" async="" src="https://ssl.google-analytics.com/ga.js"></script><script src="/files/templateArtifacts.js?1731075717"></script>
+<script>
+var STATIC_BASE = '//cdn1.editmysite.com/';
+var ASSETS_BASE = '//cdn2.editmysite.com/';
+var STYLE_PREFIX = 'wsite';
+</script>
+<script src="https://cdn2.editmysite.com/js/jquery-1.8.3.min.js"></script>
+
+<script type="text/javascript" src="//cdn2.editmysite.com/js/lang/en/stl.js?buildTime=1731026547&amp;"></script>
+<script src="//cdn2.editmysite.com/js/site/main.js?buildTime=1731026547"></script><script type="text/javascript"></script>
+//* Deuxième syntax.
+
+function initCustomerAccountsModels() {
+    (function(){_W.setup_rpc({"url":"\/ajax\/api\/JsonRPC\/CustomerAccounts\/","actions":{"CustomerAccounts":[{"name":"login","len":2,"multiple":false,"standalone":false},{"name":"logout","len":0,"multiple":false,"standalone":false},{"name":"getSessionDetails","len":0,"multiple":false,"standalone":false},{"name":"getAccountDetails","len":0,"multiple":false,"standalone":false},{"name":"getOrders","len":0,"multiple":false,"standalone":false},{"name":"register","len":4,"multiple":false,"standalone":false},{"name":"emailExists","len":1,"multiple":false,"standalone":false},{"name":"passwordReset","len":1,"multiple":false,"standalone":false},{"name":"passwordUpdate","len":3,"multiple":false,"standalone":false},{"name":"validateSession","len":1,"multiple":false,"standalone":false}]},"namespace":"_W.CustomerAccounts.RPC"});
+_W.setup_model_rpc({"rpc_namespace":"_W.CustomerAccounts.RPC","model_namespace":"_W.CustomerAccounts.BackboneModelData","collection_namespace":"_W.CustomerAccounts.BackboneCollectionData","bootstrap_namespace":"_W.CustomerAccounts.BackboneBootstrap","models":{"CustomerAccounts":{"_class":"CustomerAccounts.Model.CustomerAccounts","defaults":null,"validation":null,"types":null,"idAttribute":null,"keydefs":null}},"collections":{"CustomerAccounts":{"_class":"CustomerAccounts.Collection.CustomerAccounts"}},"bootstrap":[]});
+})();
+}
+if(document.createEvent && document.addEventListener) {
+var initEvt = document.createEvent('Event');
+initEvt.initEvent('customerAccountsModelsInitialized', true, false);
+document.dispatchEvent(initEvt);
+} else if(document.documentElement.initCustomerAccountsModels === 0){
+document.documentElement.initCustomerAccountsModels++
+}
+</script>
+<script type="text/javascript"> _W = _W || {}; _W.securePrefix='379758801672282557.weebly.com'; </script><script>_W = _W || {};
+_W.customerLocale = "en_GB";
+_W.storeName = null;
+_W.isCheckoutReskin = false;
+_W.storeCountry = "GB";
+_W.storeCurrency = "GBP";
+_W.storeEuPrivacyPolicyUrl = "";
+com_currentSite = "645213131713348032";
+com_userID = "131067957";</script><script type="text/javascript">_W.configDomain = "www.weebly.com";</script><script>_W.relinquish && _W.relinquish()</script>
+<script type="text/javascript" src="//cdn2.editmysite.com/js/lang/en/stl.js?buildTime=1731026547&amp;"></script><script> _W.themePlugins = [];</script><script type="text/javascript"> _W.recaptchaUrl = "https://www.google.com/recaptcha/api.js"; </script><script type="text/javascript"><!--
+
+
+function initFlyouts(){
+initPublishedFlyoutMenus(
+[{"id":"340104460448729694","title":"Category","url":"index.html","target":"","nav_menu":false,"nonclickable":false},{"id":"548783447232251619","title":"video","url":"video.html","target":"","nav_menu":false,"nonclickable":false},{"id":"234736574655304115","title":"new sales","url":"new-sales.html","target":"","nav_menu":false,"nonclickable":false}],
+"234736574655304115",
+'',
+'active',
+false,
+{"navigation\/item":"<li {{#id}}id=\"{{id}}\"{{\/id}} class=\"wsite-menu-item-wrap {{#has_children}}has-submenu{{\/has_children}}\">\n  <a\n    {{^nonclickable}}\n      {{^nav_menu}}\n        href=\"{{url}}\"\n      {{\/nav_menu}}\n    {{\/nonclickable}}\n    {{#target}}\n      target=\"{{target}}\"\n    {{\/target}}\n    {{#membership_required}}\n      data-membership-required=\"{{.}}\"\n    {{\/membership_required}}\n    {{#nonclickable}}\n      class=\"wsite-menu-item dead-link\"\n    {{\/nonclickable}}\n    {{^nonclickable}}\n      class=\"wsite-menu-item\"\n    {{\/nonclickable}}\n    >\n    {{{title_html}}}\n  <\/a>\n  {{#has_children}}{{> navigation\/flyout\/list}}{{\/has_children}}\n<\/li>\n","navigation\/flyout\/list":"<div class=\"wsite-menu-wrap\" style=\"display:none\">\n\t<ul class=\"wsite-menu\">\n\t\t{{#children}}{{> navigation\/flyout\/item}}{{\/children}}\n\t<\/ul>\n<\/div>\n","navigation\/flyout\/item":"<li {{#id}}id=\"{{id}}\"{{\/id}}\n  class=\"wsite-menu-subitem-wrap {{#is_current}}wsite-nav-current{{\/is_current}} {{#has_children}}has-submenu{{\/has_children}}\"\n  >\n  <a\n    {{^nonclickable}}\n      {{^nav_menu}}\n        href=\"{{url}}\"\n      {{\/nav_menu}}\n    {{\/nonclickable}}\n    {{#target}}\n      target=\"{{target}}\"\n    {{\/target}}\n    {{#nonclickable}}\n      class=\"wsite-menu-item dead-link\"\n    {{\/nonclickable}}\n    {{^nonclickable}}\n      class=\"wsite-menu-item\"\n    {{\/nonclickable}}\n    >\n    <span class=\"wsite-menu-title\">\n      {{{title_html}}}\n    <\/span>\n  <\/a>\n  {{#has_children}}{{> navigation\/flyout\/list}}{{\/has_children}}\n<\/li>\n"},
+{"hasCustomMinicart":true}
+)
+}
+//-->
+</script>
+//* Troisième syntax.
+
+<script type="text/javascript" src="//cdn2.editmysite.com/js/site/footerSignup.js?buildTime=1733442761"></script>
+<script type="text/javascript">
+if (document.readystate === 'complete') {
+    Weebly.footer.setupContainer('cdn2.editmysite.com', '1733442761');
+} else {
+    document.addEventListener('DOMContentLoaded', function() {
+        Weebly.footer.setupContainer('cdn2.editmysite.com', '1733442761');
+    });
+}
+</script>
+//*quatrième syntax
+<script src="/files/theme/plugins.js?1720602719"></script>
+//* troisième syntax.
+
+<script src="/files/theme/custom-1.js?1720602719"></script>
+<div id="customer-accounts-app"></div>
+<script src="//cdn2.editmysite.com/js/site/main-customer-accounts-site.js?buildTime=1731026547"></script>
+
+    <script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-7870337-1']);
+_gaq.push(['_setDomainName', 'none']);
+_gaq.push(['_setAllowLinker', true]);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    // NOTE: keep the [1] if you replace this code. Otherwise cookie banner scripts won't be first on the page
+    var s = document.getElementsByTagName('script')[1]; s.parentNode.insertBefore(ga, s);
+})();
+
+_W.Analytics = _W.Analytics || {'trackers': {}};
+_W.Analytics.trackers.wGA = '_gaq';
+</script>
+
+<script type="text/javascript" async="1">
+// NOTE: keep the getElementsByTagName(o)**[1]** if you replace this code. Otherwise cookie banner scripts won't be first on the page
+;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
+        p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
+        };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[1];n.async=1;
+        n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,'script','//cdn2.editmysite.com/js/wsnbn/snowday262.js','snowday'));
+
+var r = [99, 104, 101, 99, 107, 111, 117, 116, 46, 40, 119, 101, 101, 98, 108, 121, 124, 101, 100, 105, 116, 109, 121, 115, 105, 116, 101, 41, 46, 99, 111, 109];
+var snPlObR = function(arr) {
+    var s = '';
+    for (var i = 0 ; i < arr.length ; i++){
+        s = s + String.fromCharCode(arr[i]);
+    }
+    return s;
+};
+var s = snPlObR(r);
+
+var regEx = new RegExp(s);
+
+_W.Analytics = _W.Analytics || {'trackers': {}};
+_W.Analytics.trackers.wSP = 'snowday';
+_W.Analytics.user_id = '131067957';
+_W.Analytics.site_id = '645213131713348032';
+
+var drSegmentsTag = document.getElementById('drSegments');
+if (drSegmentsTag) {
+    _W.Analytics.spContexts = _W.Analytics.spContexts || [];
+
+    var segmentData = JSON.parse(drSegmentsTag.innerText);
+    segmentData.forEach(function(test) {
+        _W.Analytics.spContexts.push({
+            schema: "iglu:com.weebly/context_ab_segment/jsonschema/1-0-0",
+            data: {
+                test_id: test.name,
+                segment: test.variant,
+            }
+        });
+    });
+}
+
+
+(function(app_id, ec_hostname, discover_root_domain) {
+    var track = window[_W.Analytics.trackers.wSP];
+    if (!track) return;
+    track('newTracker', app_id, ec_hostname, {
+        appId: app_id,
+        post: true,
+        platform: 'web',
+        discoverRootDomain: discover_root_domain,
+        cookieName: '_snow_',
+        contexts: {
+            webPage: true,
+            performanceTiming: true,
+            gaCookies: true
+        },
+        crossDomainLinker: function (linkElement) {
+            return regEx.test(linkElement.href);
+        },
+        respectDoNotTrack: true
+    });
+    track('trackPageView', _W.Analytics.user_id+':'+_W.Analytics.site_id, _W.Analytics.spContexts);
+    track('crossDomainLinker', function (linkElement) {
+        return regEx.test(linkElement.href);
+    });
+})(
+    '_wn',
+    'ec.editmysite.com',
+    false
+);
+</script>
+
+
+
